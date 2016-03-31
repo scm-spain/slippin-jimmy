86cf1be718e59e98e4542046c80f8753791f5203
Javi Martín <webmasterhhp@gmail.com>
2016-03-31 11:56:57 +0200

    closes #1 Setup.py modified to upload package to PyPi

--- setup.py
+++ setup.py
@@ -3,19 +3,38 @@
 """
 @author: SCMSpain
 """
+from codecs import open
+from os import path
 
 from setuptools import setup, find_packages
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='slippinj',
-    version='1.1.0',
+    version='1.1.1',
     author='Data Architects SCM Spain',
     author_email='data.architects@scmspain.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    url='https://github.schibsted.io/scmspain/data-bi-slippin-jimmy',
+    url='https://github.com/scm-spain/slippin-jimmy',
     description='Tools to generate and deploy Apache Oozie workflows',
+    long_description=long_description,
+    license='GPLv2',
     install_requires=open('requirements.txt').read().split(),
-    scripts=['scripts/jimmy']
+    scripts=['scripts/jimmy'],
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Code Generators',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Intended Audience :: Developers',
+        'Environment :: Console'
+    ],
+    keywords='oozie workflows code generation emr aws'
 )
